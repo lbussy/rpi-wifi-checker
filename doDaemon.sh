890d346e8947394a49ddf278f7dbe7a4e1b236b3
lbussy <lee@bussy.org>
2019-03-23 16:52:01 -0500

    Typo in global decls part deux

--- doDaemon.sh
+++ doDaemon.sh
@@ -16,7 +16,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-# Location for check script instalation
+# Global constant declarations
 declare THISSCRIPT SCRIPTLOC RUNSCRIPT DAEMONNAME RUNAS URL SCRIPTURL
 
 ############
