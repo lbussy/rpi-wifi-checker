223fa70fff23f21cf6290d9a15c1ab48cba4f096
lbussy <lee@bussy.org>
2019-03-23 16:50:55 -0500

    Typo in global decls

--- doDaemon.sh
+++ doDaemon.sh
@@ -17,7 +17,7 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 # Location for check script instalation
-define THISSCRIPT SCRIPTLOC RUNSCRIPT DAEMONNAME RUNAS URL SCRIPTURL
+declare THISSCRIPT SCRIPTLOC RUNSCRIPT DAEMONNAME RUNAS URL SCRIPTURL
 
 ############
 ### Init
